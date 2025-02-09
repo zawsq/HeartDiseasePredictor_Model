2c9ee619b91d56607ee3d6c81146244984454058
L <60060180+zawsq@users.noreply.github.com>
2025-02-09 12:49:28 +0800

    Update logger.py
    Remove write

--- src/logger.py
+++ src/logger.py
@@ -1,35 +1,15 @@
 import logging
-import os
-from datetime import datetime
-from logging.handlers import RotatingFileHandler
-
-# Define log file directory and filename
-logs_path = os.path.join(os.getcwd(), "logs")
-os.makedirs(logs_path, exist_ok=True)
-
-# Log file name with timestamp for the current day
-LOG_FILE = f"log_{datetime.now().strftime('%m_%d_%Y')}.log"
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
-
-# Setting up the rotating file handler
-log_handler = RotatingFileHandler(
-    LOG_FILE_PATH,
-    maxBytes=10 * 1024 * 1024,  # Maximum size (10 MB)
-    backupCount=5,  # Keep 5 backup files
-)
-
-log_handler.setFormatter(
-    logging.Formatter('[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s')
-)
+from logging import StreamHandler
 
 # Set up the logger
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-logger.addHandler(log_handler)
 
-# Optional: Add console handler for real-time output in the terminal
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(logging.Formatter('[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s'))
+# Add console handler for real-time output in the terminal
+console_handler = StreamHandler()
+console_handler.setFormatter(
+    logging.Formatter('[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s')
+)
 logger.addHandler(console_handler)
 
 # Test logging
