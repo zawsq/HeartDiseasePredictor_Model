499768d8611630a7e4ec520a3f78fec5488b23af
L <60060180+zawsq@users.noreply.github.com>
2025-02-09 13:03:33 +0800

    Update app.py

--- deployment/Api/app.py
+++ deployment/Api/app.py
@@ -52,7 +52,7 @@ def test():
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=7860, log_level="info", reload=True)
 
 
 
